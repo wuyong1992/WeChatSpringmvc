315a9202741a6b3db08ac1f850a3fdfd7abf7cf3
wuyong1992 <627522616@qq.com>
2017-04-06 20:53:25 +0800

    日期可以存储，但是数据不对。 pom文件中添加以JDK1.8版本编译的代码

--- src/main/java/sql/wechat.sql
+++ src/main/java/sql/wechat.sql
@@ -18,11 +18,11 @@ USE `wechatspringmvc`;
 
 -- 导出  表 wechatspringmvc.userinfo 结构
 CREATE TABLE IF NOT EXISTS `userinfo` (
-  `sex` tinyint(4) DEFAULT NULL,
-  `language` varchar(255) DEFAULT NULL COMMENT '语言',
   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '用户id',
   `openId` varchar(255) DEFAULT NULL COMMENT '用户openid',
   `unionId` varchar(255) DEFAULT NULL COMMENT '用户唯一unionId',
+  `sex` tinyint(4) DEFAULT NULL,
+  `language` varchar(255) DEFAULT NULL COMMENT '语言',
   `username` varchar(255) DEFAULT NULL COMMENT '用户名',
   `phoneNum` varchar(20) DEFAULT NULL COMMENT '手机号',
   `city` varchar(255) DEFAULT NULL COMMENT '城市',
@@ -30,6 +30,8 @@ CREATE TABLE IF NOT EXISTS `userinfo` (
   `country` varchar(255) DEFAULT NULL COMMENT '国家',
   `isSubscribe` tinyint(4) DEFAULT '0' COMMENT '0表示没有关注，1表示关注，默认为0',
   `isMember` tinyint(4) DEFAULT '0' COMMENT '1表示是，0表示否，默认为不是会员',
+  `registerTime` datetime DEFAULT NULL,
+  `vipTime` datetime DEFAULT NULL,
   `userPortrait` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COMMENT='用户基本信息表';
